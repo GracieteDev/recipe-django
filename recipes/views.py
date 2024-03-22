21ea8c05e82eabf6d187435ecc6a5cd1b8fbf2c5
Graciete Gil <graciete.gil@gmail.com>
2024-03-22 01:05:30 +0000

    Create edit recipe template

--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import  CreateView,ListView, DetailView, DeleteView
+from django.views.generic import (CreateView,ListView, DetailView, DeleteView, UpdateView)
 
 from .models import Recipe
 from .forms import RecipeForm
@@ -42,6 +42,24 @@ class AddRecipe(LoginRequiredMixin, CreateView):
         form.instance.user = self.request.user
         return super(AddRecipe,self).form_valid(form)  
     
+
+class EditRecipe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    Edit a recipe
+    """
+    
+    model = Recipe
+    form_class = RecipeForm
+    success_url = '/recipes/'
+    template_name ='recipes/edit_recipe.html'
+    
+
+    def test_func(self):
+        return self.request.user == self.get_object().user  
+
+
+
+
     
 class DeleteRecipe(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     """
