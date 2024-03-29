e487bf1c98ca4f81ca16737c9ca42012cd088780
Graciete Gil <graciete.gil@gmail.com>
2024-03-29 13:04:38 +0000

    update settings

--- main/settings.py
+++ main/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = False
+DEBUG = os.environ.get("DEVELOPMENT", "False").lower() == "true"
 
 
 ALLOWED_HOSTS = [
