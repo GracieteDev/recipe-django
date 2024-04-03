9874112b94db00e987059cd340c2e8d47d958945
Graciete Gil <graciete.gil@gmail.com>
2024-04-03 18:57:29 +0100

    update file

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEVELOPMENT", "False").lower() == "true"
-DEBUG_PROPAGATE_EXCEPTIONS = True
+
 
 
 ALLOWED_HOSTS = [
